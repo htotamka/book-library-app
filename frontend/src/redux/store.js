a08f497f796e5227e02fde064b264cec3b6fd735
Nataliia Yurchenko <htotamka@gmail.com>
2024-12-09 15:37:39 +0000

    Filter Redux slice and Filter by title

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/reducer';
+import filterReducer from './slices/filterSlice';
+
 
 
 const store = configureStore({
   reducer: {
-    books: booksReducer
+    books: booksReducer,
+    filter: filterReducer
   }
 });
 
