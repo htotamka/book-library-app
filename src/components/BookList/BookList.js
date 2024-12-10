eb9e369681bdf85b8ccc8fdbc328257f6a01d5c7
Nataliia Yurchenko <htotamka@gmail.com>
2024-12-10 10:45:52 +0000

    Highlight filter text matches

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -28,6 +28,22 @@ const handleToggleFavorite = (id) => {
 		return matchesTitle && matchesAuthor && matchesFavorite
 	})
 
+	const highlightMatch = (text, filter) => {
+		if (!filter) return text
+
+		const regex = new RegExp(`(${filter})`, 'gi')
+		return text.split(regex).map((substring, i) => {
+			if (substring.toLowerCase() === filter.toLowerCase()) {
+				return (<span key={i} className='highlight'>
+					{substring}
+				</span>
+		)
+			}
+			return substring
+		})
+			
+		}
+
 
   return (
 		<div className='app-block book-list'>
@@ -39,14 +55,19 @@ const handleToggleFavorite = (id) => {
 					{filteredBooks.map((book, i) => (
 						<li key={book.id}>
 							<div className='book-info'>
-								{++i}. {book.title} by <strong>{book.author}</strong>
+								{++i}. {highlightMatch(book.title, titleFilter)} by{' '}
+								<strong>{highlightMatch(book.author, authorFilter)}</strong>
 							</div>
-              <div className='book-actions'>
-                <span onClick={() => handleToggleFavorite(book.id)}>
-                {book.isFavorite ? <BsBookmarkStarFill className='star-icon'/> : <BsBookmarkStar className='star-icon'/>}
-                </span>
-                <button onClick={() => handleDelete(book.id)}>Delete</button>
-              </div>
+							<div className='book-actions'>
+								<span onClick={() => handleToggleFavorite(book.id)}>
+									{book.isFavorite ? (
+										<BsBookmarkStarFill className='star-icon' />
+									) : (
+										<BsBookmarkStar className='star-icon' />
+									)}
+								</span>
+								<button onClick={() => handleDelete(book.id)}>Delete</button>
+							</div>
 						</li>
 					))}
 				</ul>
