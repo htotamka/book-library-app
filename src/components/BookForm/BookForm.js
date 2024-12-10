2f974bedfcf49af34f341a41cceeedb4fce5dc48
Nataliia Yurchenko <htotamka@gmail.com>
2024-12-10 11:43:49 +0000

    Created books slice instead of regular reducer

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 
-import { addBook } from '../../redux/books/actionCreators'
+import { addBook } from '../../redux/slices/booksSlice'
 import createBookWithID from '../../utils/createBookWithID.js'
 import booksData from '../../data/books.json'
 import './BookForm.css'
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,13 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
 
-import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
+import { deleteBook, toggleFavorite, selectBooks } from '../../redux/slices/booksSlice'
 import { selectTitleFilter, selectAuthorFilter, selectOnlyFavoriteFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 
 const BookList = () => {
-  const books = useSelector((state) => state.books)
+  const books = useSelector(selectBooks)
 	const titleFilter = useSelector(selectTitleFilter)
 	const authorFilter = useSelector(selectAuthorFilter)
 	const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter)
