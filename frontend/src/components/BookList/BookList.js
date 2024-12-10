e7fa47ba1afd0d27f13552fa5c5201b3a6f7b1fa
Nataliia Yurchenko <htotamka@gmail.com>
2024-12-10 09:26:11 +0000

    Filter by author

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -2,13 +2,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
 
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
-import { selectTitleFilter } from '../../redux/slices/filterSlice'
+import { selectTitleFilter, selectAuthorFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 
 const BookList = () => {
   const books = useSelector((state) => state.books)
 	const titleFilter = useSelector(selectTitleFilter)
+	const authorFilter = useSelector(selectAuthorFilter)
   const dispatch = useDispatch()
 
   const handleDelete = (id) => {
@@ -21,7 +22,8 @@ const handleToggleFavorite = (id) => {
 
 	const filteredBooks = books.filter((book) => {
 		const matchesTitle = book.title.toLowerCase().includes(titleFilter.toLowerCase())
-		return matchesTitle
+		const matchesAuthor = book.author.toLowerCase().includes(authorFilter.toLowerCase())
+		return matchesTitle && matchesAuthor
 	})
 
 
