2feb254147c4c523b4eac2741228223ef8f01679
Nataliia Yurchenko <htotamka@gmail.com>
2024-12-14 14:05:43 +0000

    Added isLoading and spinner in the BookForm

--- api/index.js
+++ api/index.js
@@ -6,12 +6,24 @@ const app = express();
 
 app.use(cors());
 
-app.get('/random-book', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * booksData.length);
-  const randomBook = booksData[randomIndex];
+function getRandomBook() {
+  const randomIndex = Math.floor(Math.random() * booksData.length)
+	const randomBook = booksData[randomIndex]
+  return randomBook
+}
 
-  res.json(randomBook);
+app.get('/random-book', (req, res) => {
+
+  res.json(getRandomBook())
 });
+
+app.get('/random-book-delayed', (req, res) => {
+
+  setTimeout(() => {
+    res.json(getRandomBook())
+  }, 2000)
+	
+})
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
